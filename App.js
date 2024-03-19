9e456e8abcfcf5bb2243d49e086b38e09dad3e67
dolly596 <83473173+dolly596@users.noreply.github.com>
2024-03-19 16:45:24 -0300

    Primeira atividade android

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, Image } from 'react-native';
+import { styles } from './AppStyle';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <StatusBar backgroundColor="#00E5E5" barStyle="dark-content" />
+      <Image style={styles.imagens}
+        source={require('./assets/Astronauta.jpg')}
+      />
+         <Text style={styles.texto}>Esse é um pequeno passo para o homem,
+         mas um gigantesco salto para a humanidade</Text>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ AppStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'black',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    imagens: {
+      height:620,
+      marginTop: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    texto: {
+      color: "white",
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+      marginLeft: 20,
+      marginRight:20,
+      marginTop: 30,
+      fontSize: 23
+    }
+  });
